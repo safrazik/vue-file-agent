0e595e5077771824798ecc425efd6db98b053cc8
seriouslag <Landon.Gavin@gmail.com>
2019-09-02 12:10:04 -0400

    Added missing type

--- src/index.d.ts
+++ src/index.d.ts
@@ -12,43 +12,47 @@ interface Options {
   thumbnailSize: undefined|number;
 }
 
-interface errorText {
+interface ErrorText {
   commong?: string;
   type?: string;
   size?: string;
 }
 
-class FileData {
-  public raw: any;
-  public file: File|any;
-  public url: null|string;
-  public urlResized: null|string;
-  public lastKnownSrc: null|any;
-  public _progress: ProgressEvent|boolean;
+export class FileData {
+  public constructor(data: RawFileData, options: any);
+  public static fromRaw: (fileDataRaw: RawFileData, options: any) => Promise<FileData>;
+  public static fromRawArray: (filesDataRaw: RawFileData[], options: any) => Promise<FileData[]>;
+  public static toRawArray: (filesData: FileData[]) => RawFileData[];
+  public static readFile: (fileData: FileData) => Promise<FileData>;
+  public static readFiles: (filesData: FileData[]) => Promise<FileData[]>;
+  public raw: RawFileData;
+  public url: null | string;
+  public urlResized: null | string;
+  public _progress: ProgressEvent | boolean;
   public accept: string;
   public dimensions: Dimensions;
   public error: boolean;
-  public file: File;
-  public height: undefined|number|string;
-  public width: undefined|number|string;
+  public file: File | any;
+  public height: undefined | number | string;
+  public width: undefined | number | string;
   public id: string;
   public image: HTMLImageElement;
   public imageColor: number[];
   public isPlayingAv: false;
-  public lastKnownSrc: null|string;
-  public maxSize: null|string;
+  public lastKnownSrc: null | string;
+  public maxSize: null | string;
   public options: Options;
   public read: boolean;
   public thumbnailSize: number;
   public upload: any;
   public videoThumbnail: any;
-  public populate: (data: any, options = {}) => void;
+  public populate: (data: RawFileData, options: any) => void;
   public hasProgress: () => boolean;
-  public progress: (value: undefined|number) => any|number;
-  public size: () => number|'';
+  public progress: (value: undefined | number) => any | number;
+  public size: () => number | '';
   public src: () => string;
   public ext: () => string;
-  public name: () => string;
+  public name: (withoutName?: boolean) => string;
   public isDarkColor: () => boolean;
   public color: () => string;
   public isImage: () => boolean;
@@ -60,18 +64,30 @@ class FileData {
   public setUrl: (url: string) => Promise<any>;
   public imageResized: (resized: any) => void;
   public resizeImage: () => Promise<void>;
-  public icon: () => SvgPath;
-  public getErrorMessage: (errorText: string) => errorText;
-  public toRaw: () => any;
+  public icon: () => any;
+  public getErrorMessage: (errorText: string) => ErrorText;
+  public toRaw: () => RawFileData;
   public validate: () => void;
-  static fromRaw: (fileDataRaw: any, options: any) => Promise<any>;
-  static fromRawArray: (filesDataRaw: any[], options) => Promise<any[]>
-  static toRawArray: (filesData: any) => any[];
-  static readFile: (fileData: any) => Promise<any>;
-  static readFiles: (filesData: any) => Promise<any[]>;
 }
 
-export { FileData, Dimensions, Options }
+interface RawFileData {
+  url: string | null;
+  urlResized: string | null;
+  src: () => any;
+  name: string;
+  lastModified: number;
+  sizeText: string;
+  size: number;
+  type: string;
+  ext: string;
+  color: string;
+  file: File;
+  progress: () => any;
+  error: boolean;
+  dimensions: Dimensions;
+}
+
+export { Dimensions, Options, RawFileData };
 export default class VueFileAgentPlugin {
   static install: PluginFunction<never>;
   static VueFileIcon: Vue;
