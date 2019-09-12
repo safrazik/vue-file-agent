c24452b33c78d626eff918396c7f8a2d34baecfb
Safraz Razik <safrazik@gmail.com>
2019-09-12 16:25:32 +0530

    Improve delete/edit icons

--- src/components/vue-file-preview.vue
+++ src/components/vue-file-preview.vue
@@ -22,10 +22,13 @@
       <span class="file-ext">{{ fileData.ext() }}</span>
       <span class="file-size">{{ fileData.size() }}</span>
       <span v-if="deletable" class="file-delete" @click="removeFileData(fileData)" @touchstart="filenameClearPressed()" @mousedown="filenameClearPressed()" >
-        &times;
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
       </span>
       <span class="file-name" @click="editFileName()">
-        <input class="file-name-input" ref="input" v-if="editable === true" :disabled="disabled === true" type="text" :value="fileData.name(true)" @focus="editInputFocused()" @blur="editInputBlured()" @change="filenameChanged()" @input="filenameChanged()" @keyup.enter="filenameChanged(true)">
+        <input class="file-name-input" ref="input" v-if="editable === true" :disabled="disabled === true" type="text" :value="fileData.name(true)" @focus="editInputFocused()" @blur="editInputBlured()" @change="filenameChanged()" @input="filenameChanged()" @keyup.enter="filenameChanged(true)" @keyup.esc="filenameChanged(false)">
+        <span class="file-name-edit-icon" v-if="editable === true">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+        </span>
         <span class="file-name-text">{{ fileData.name(true) }}</span>
       </span>
       <span v-if="fileData.dimensions.width && fileData.dimensions.height" class="image-dimension">
@@ -140,6 +143,9 @@
       filenameChanged(completed){
         if(completed){
           this.$refs.input.blur();
+        }
+        if(completed === false){
+          this.clearFilename();
         }
       },
 
