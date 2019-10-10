4493445826a4127f276d1648eafa53dcec4b81da
Safraz Razik <safrazik@gmail.com>
2019-10-11 00:06:14 +0530

    Update test

--- tests/unit/vue-file-preview.vue.spec.js
+++ tests/unit/vue-file-preview.vue.spec.js
@@ -3,17 +3,30 @@ import FileData from '@/lib/file-data'
 import helper from './helper'
 
 describe('vue-file-preview.vue', () => {
+
+  var rawFileData = {
+    "name":"sample.pdf",
+    "lastModified":1565232623243,
+    // "sizeText":"3 KB",
+    "size":3028,
+    "type":"application/pdf",
+    "ext":"pdf"
+  };
+
   it('renders FileData when passed', () => {
-    var fileData = new FileData({
-      "name":"sample.pdf",
-      "lastModified":1565232623243,
-      // "sizeText":"3 KB",
-      "size":3028,
-      "type":"application/pdf",
-      "ext":"pdf"
-    });
+    var fileData = new FileData(rawFileData);
     var wrapper = helper.getWrapper(VueFilePreview, {
-      fileData: fileData,
+      value: fileData,
+    }, {})
+    expect(helper.getText(wrapper, '.file-name')).toBe('sample')
+    expect(helper.getText(wrapper, '.file-ext')).toBe('pdf')
+    expect(helper.getText(wrapper, '.file-size')).toBe('3 KB')
+  });
+
+  it('renders raw FileData when passed', () => {
+    var fileData = rawFileData;
+    var wrapper = helper.getWrapper(VueFilePreview, {
+      value: fileData,
     }, {})
     expect(helper.getText(wrapper, '.file-name')).toBe('sample')
     expect(helper.getText(wrapper, '.file-ext')).toBe('pdf')
