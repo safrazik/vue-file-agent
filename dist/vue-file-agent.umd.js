15cca450bb177f37d4f9ee47b98b791bb653755c
Safraz Razik <safrazik@gmail.com>
2019-11-26 12:58:14 +0530

    Add dist files for v1.3.4

--- dist/vue-file-agent.umd.js
+++ dist/vue-file-agent.umd.js
@@ -4686,8 +4686,121 @@ function () {
       });
     }
   }, {
+    key: "getImageOrientationFromArrayBuffer",
+    value: function getImageOrientationFromArrayBuffer(buffer) {
+      // -2: not jpeg
+      // -1: not defined
+      var view = new DataView(buffer);
+
+      if (view.getUint16(0, false) != 0xFFD8) {
+        return -2;
+      }
+
+      var length = view.byteLength,
+          offset = 2;
+
+      while (offset < length) {
+        if (view.getUint16(offset + 2, false) <= 8) {
+          return -1;
+        }
+
+        var marker = view.getUint16(offset, false);
+        offset += 2;
+
+        if (marker == 0xFFE1) {
+          if (view.getUint32(offset += 2, false) != 0x45786966) {
+            return -1;
+          }
+
+          var little = view.getUint16(offset += 6, false) == 0x4949;
+          offset += view.getUint32(offset + 4, little);
+          var tags = view.getUint16(offset, little);
+          offset += 2;
+
+          for (var i = 0; i < tags; i++) {
+            if (view.getUint16(offset + i * 12, little) == 0x0112) {
+              return view.getUint16(offset + i * 12 + 8, little);
+            }
+          }
+        } else if ((marker & 0xFF00) != 0xFF00) {
+          break;
+        } else {
+          offset += view.getUint16(offset, false);
+        }
+      }
+
+      return -1;
+    }
+  }, {
+    key: "getImageOrientation",
+    value: function getImageOrientation(file) {
+      var _this2 = this;
+
+      return new Promise(function (resolve, reject) {
+        var reader = new FileReader();
+
+        if (!reader.readAsArrayBuffer) {
+          return resolve(-3);
+        }
+
+        reader.onload = function (e) {
+          resolve(_this2.getImageOrientationFromArrayBuffer(e.target.result));
+        }; // reader.readAsArrayBuffer(file);
+        // Exif should appear in the first 64 KB, however this is not guaranteed—it may be possible for the Exif to extend beyond (stackoverflow.com/questions/3248946/…). Therefore we have to read the whole file for safety. Alternatively, instead of reading the file as an ArrayBuffer, we could use streams (where supported) so we lazily read the file until the Exif has been found: mobile.twitter.com/jaffathecake/status/1085443592678752256. If anyone has success with this approach, please share! – Oliver Joseph Ash
+
+
+        reader.readAsArrayBuffer(file.slice(0, 65536));
+      });
+    }
+  }, {
+    key: "rotateCanvas",
+    value: function rotateCanvas(srcOrientation, canvas, ctx, width, height) {
+      // set proper canvas dimensions before transform & export
+      if (4 < srcOrientation && srcOrientation < 9) {
+        canvas.width = height;
+        canvas.height = width;
+      } else {
+        canvas.width = width;
+        canvas.height = height;
+      } // transform context before drawing image
+
+
+      switch (srcOrientation) {
+        case 2:
+          ctx.transform(-1, 0, 0, 1, width, 0);
+          break;
+
+        case 3:
+          ctx.transform(-1, 0, 0, -1, width, height);
+          break;
+
+        case 4:
+          ctx.transform(1, 0, 0, -1, 0, height);
+          break;
+
+        case 5:
+          ctx.transform(0, 1, 1, 0, 0, 0);
+          break;
+
+        case 6:
+          ctx.transform(0, 1, -1, 0, height, 0);
+          break;
+
+        case 7:
+          ctx.transform(0, -1, -1, 0, height, width);
+          break;
+
+        case 8:
+          ctx.transform(0, -1, 1, 0, 0, width);
+          break;
+
+        default:
+          break;
+      }
+    }
+  }, {
     key: "getImageResized",
-    value: function getImageResized(image, widthLimit, heightLimit) {
+    value: function getImageResized(image, widthLimit, heightLimit, orientation) {
       var width = image.width;
       var height = image.height;
       var thumbnailSize = widthLimit;
@@ -4712,9 +4825,10 @@ function () {
       width = Math.floor(width);
       height = Math.floor(height);
       var canvas = document.createElement('canvas');
+      var context = canvas.getContext('2d');
       canvas.width = width;
       canvas.height = height;
-      var context = canvas.getContext('2d');
+      this.rotateCanvas(orientation, canvas, context, width, height);
       context.drawImage(image, 0, 0, width, height);
       var avgColor = null;
 
@@ -4738,11 +4852,11 @@ function () {
   }, {
     key: "resizeImageUrl",
     value: function resizeImageUrl(image, url, thumbnailSize) {
-      var _this2 = this;
+      var _this3 = this;
 
       return new Promise(function (resolve, reject) {
         image.onload = function () {
-          var resized = _this2.getImageResized(image, thumbnailSize);
+          var resized = _this3.getImageResized(image, thumbnailSize);
 
           resolve(resized);
         };
@@ -4753,7 +4867,7 @@ function () {
   }, {
     key: "resizeImageFile",
     value: function resizeImageFile(image, file, thumbnailSize) {
-      var _this3 = this;
+      var _this4 = this;
 
       return new Promise(function (resolve, reject) {
         if (file.type.indexOf('image') == -1) {
@@ -4765,14 +4879,18 @@ function () {
         var revokeObjectURL = (window.URL || window['webkitURL'] || {}).revokeObjectURL;
         var shouldRevoke = false;
 
+        var orientationPromise = _this4.getImageOrientation(file);
+
         image.onload = function () {
-          var resized = _this3.getImageResized(image, thumbnailSize);
+          orientationPromise.then(function (orientation) {
+            var resized = _this4.getImageResized(image, thumbnailSize, null, orientation);
 
-          if (shouldRevoke) {
-            revokeObjectURL(image.src);
-          }
+            if (shouldRevoke) {
+              revokeObjectURL(image.src);
+            }
 
-          resolve(resized);
+            resolve(resized);
+          });
         };
 
         if (!(file instanceof File)) {
@@ -4785,7 +4903,7 @@ function () {
           return;
         }
 
-        _this3.getDataURL(file).then(function (dataUrl) {
+        _this4.getDataURL(file).then(function (dataUrl) {
           image.src = dataUrl;
         });
       });
