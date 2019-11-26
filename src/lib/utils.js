d25257f61188ac8ecdd9fca4cb49fadb904baf77
Safraz Razik <safrazik@gmail.com>
2019-11-26 12:48:35 +0530

    Fix #28 Wrong Orientation in images

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -87,7 +87,93 @@ class Utils {
     });
   }
 
-  getImageResized(image, widthLimit, heightLimit){
+  getImageOrientationFromArrayBuffer(buffer){
+    // -2: not jpeg
+    // -1: not defined
+    var view = new DataView(buffer);
+    if (view.getUint16(0, false) != 0xFFD8)
+    {
+        return -2;
+    }
+    var length = view.byteLength, offset = 2;
+    while (offset < length) 
+    {
+      if (view.getUint16(offset+2, false) <= 8){
+        return -1;
+      }
+      var marker = view.getUint16(offset, false);
+      offset += 2;
+      if (marker == 0xFFE1) 
+      {
+          if (view.getUint32(offset += 2, false) != 0x45786966) 
+          {
+              return -1;
+          }
+
+          var little = view.getUint16(offset += 6, false) == 0x4949;
+          offset += view.getUint32(offset + 4, little);
+          var tags = view.getUint16(offset, little);
+          offset += 2;
+          for (var i = 0; i < tags; i++)
+          {
+              if (view.getUint16(offset + (i * 12), little) == 0x0112)
+              {
+                  return view.getUint16(offset + (i * 12) + 8, little);
+              }
+          }
+      }
+      else if ((marker & 0xFF00) != 0xFF00)
+      {
+          break;
+      }
+      else
+      { 
+          offset += view.getUint16(offset, false);
+      }
+    }
+    return -1;
+  }
+
+  getImageOrientation(file) {
+    return new Promise((resolve, reject)=> {
+      var reader = new FileReader();
+      if (!reader.readAsArrayBuffer) {
+        return resolve(-3);
+      }
+      reader.onload = (e)=> {
+        resolve(this.getImageOrientationFromArrayBuffer(e.target.result));
+      };
+      // reader.readAsArrayBuffer(file);
+      // Exif should appear in the first 64 KB, however this is not guaranteed—it may be possible for the Exif to extend beyond (stackoverflow.com/questions/3248946/…). Therefore we have to read the whole file for safety. Alternatively, instead of reading the file as an ArrayBuffer, we could use streams (where supported) so we lazily read the file until the Exif has been found: mobile.twitter.com/jaffathecake/status/1085443592678752256. If anyone has success with this approach, please share! – Oliver Joseph Ash
+      reader.readAsArrayBuffer(file.slice(0, 65536));
+    });
+  }
+
+  rotateCanvas(srcOrientation, canvas, ctx, width, height){
+    // set proper canvas dimensions before transform & export
+    if (4 < srcOrientation && srcOrientation < 9) {
+      canvas.width = height;
+      canvas.height = width;
+    } else {
+      canvas.width = width;
+      canvas.height = height;
+    }
+
+    // transform context before drawing image
+    switch (srcOrientation) {
+      case 2: ctx.transform(-1, 0, 0, 1, width, 0); break;
+      case 3: ctx.transform(-1, 0, 0, -1, width, height); break;
+      case 4: ctx.transform(1, 0, 0, -1, 0, height); break;
+      case 5: ctx.transform(0, 1, 1, 0, 0, 0); break;
+      case 6: ctx.transform(0, 1, -1, 0, height, 0); break;
+      case 7: ctx.transform(0, -1, -1, 0, height, width); break;
+      case 8: ctx.transform(0, -1, 1, 0, 0, width); break;
+      default: break;
+    }
+
+  }
+
+  getImageResized(image, widthLimit, heightLimit, orientation){
     var width = image.width;
     var height = image.height;
     var thumbnailSize = widthLimit;
@@ -113,9 +199,12 @@ class Utils {
     height = Math.floor(height);
 
     var canvas = document.createElement('canvas');
+    var context = canvas.getContext('2d');
     canvas.width = width;
     canvas.height = height;
-    var context = canvas.getContext('2d');
+
+    this.rotateCanvas(orientation, canvas, context, width, height);
+
     context.drawImage(image, 0, 0, width, height);
     var avgColor = null;
     try {
@@ -153,12 +242,15 @@ class Utils {
       var createObjectURL = (window.URL || window['webkitURL'] || {}).createObjectURL;
       var revokeObjectURL = (window.URL || window['webkitURL'] || {}).revokeObjectURL;
       var shouldRevoke = false;
+      var orientationPromise = this.getImageOrientation(file);
       image.onload = ()=> {
-        var resized = this.getImageResized(image, thumbnailSize);
-        if(shouldRevoke){
-          revokeObjectURL(image.src);
-        }
-        resolve(resized);
+        orientationPromise.then((orientation)=> {
+          var resized = this.getImageResized(image, thumbnailSize, null, orientation);
+          if(shouldRevoke){
+            revokeObjectURL(image.src);
+          }
+          resolve(resized);
+        });
       };
       if (!(file instanceof File)) {
         return reject('Invalid file object. Use url or a valid instance of File class');
