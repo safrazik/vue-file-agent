3123c7a1c2923bbd0522b7e07df63d0c4ce55aac
Safraz Razik <safrazik@gmail.com>
2019-12-16 12:54:08 +0530

    Prettier code!

--- src/components/vue-file-agent.vue
+++ src/components/vue-file-agent.vue
@@ -1,62 +1,137 @@
 <template>
-  <div @dragover="dragOver" @dragenter="dragEnter" @dragleave="dragLeave" @drop="drop" :class="['is-sortable-' + (isSortable  ? 'enabled' : 'disabled'), {'is-sortable-hold': sortable === 'hold'}, {'is-sortable-hold': sortable === 'handle'}, {'is-sortable-immediately': sortable === true}, {'is-sorting': isSorting, 'is-sorting-active': isSortingActive, 'is-drag-over': isDragging, 'is-disabled': disabled === true}, 'theme-' + theme]" :id="'vfa-' + uniqueId">
+  <div
+    @dragover="dragOver"
+    @dragenter="dragEnter"
+    @dragleave="dragLeave"
+    @drop="drop"
+    :class="[
+      'is-sortable-' + (isSortable ? 'enabled' : 'disabled'),
+      { 'is-sortable-hold': sortable === 'hold' },
+      { 'is-sortable-hold': sortable === 'handle' },
+      { 'is-sortable-immediately': sortable === true },
+      {
+        'is-sorting': isSorting,
+        'is-sorting-active': isSortingActive,
+        'is-drag-over': isDragging,
+        'is-disabled': disabled === true,
+      },
+      'theme-' + theme,
+    ]"
+    :id="'vfa-' + uniqueId"
+  >
     <slot name="before-outer"></slot>
-  <div class="grid-block-wrapper vue-file-agent vue-file-agent-light file-input-wrapper drop_zone" :class="{'is-compact': !!compact, 'is-single': !hasMultiple, 'has-multiple': hasMultiple, 'no-meta': meta === false}">
-    <slot name="before-inner"></slot>
-    <canvas ref="thumbnailCanvas" style="position: fixed; visibility: hidden; z-index: -3;"></canvas>
-    <div class="overall-progress" v-if="overallProgress" :class="{'overall-progress-full': overallProgress >= 100}">
-      <div class="overall-progress-bar" :style="{width: overallProgress + '%'}"></div>
-      <div class="overall-progress-left" :style="{width: (100 - overallProgress) + '%'}"></div>
-    </div>
+    <div
+      class="grid-block-wrapper vue-file-agent file-input-wrapper"
+      :class="{
+        'is-compact': !!compact,
+        'is-single': !hasMultiple,
+        'has-multiple': hasMultiple,
+        'no-meta': meta === false,
+      }"
+    >
+      <slot name="before-inner"></slot>
+      <canvas ref="thumbnailCanvas" style="position: fixed; visibility: hidden; z-index: -3;"></canvas>
+      <div class="overall-progress" v-if="overallProgress" :class="{ 'overall-progress-full': overallProgress >= 100 }">
+        <div class="overall-progress-bar" :style="{ width: overallProgress + '%' }"></div>
+        <div class="overall-progress-left" :style="{ width: 100 - overallProgress + '%' }"></div>
+      </div>
 
-  <component :is="isSortable ? 'vfa-sortable-list': 'VueFileList'" v-model="filesData" :axis="theme == 'list' ? 'y' : 'xy'" :appendTo="'#vfa-' + uniqueId + ' .vue-file-agent'" :transitionDurationx="transitionDuration" :pressDelay="sortable === 'hold' ? 200 : 0" :useDragHandle="sortable === 'handle'" @sort-start="sortStart()" @sort-end="sortEnd($event)" :helperClass="'active-sorting-item'">
-  <transition-group name="grid-box" tag="div" class="">
-    <!-- <template v-for="(fileData, index) in filesData"> -->
-      <component :is="isSortable ? 'vfa-sortable-item' : 'VueFileListItem'" v-for="(fileData, index) in filesData" class="file-preview-wrapper grid-box-item grid-block" :index="index" :key="fileData.id">
-        <span v-vfa-sortable-handle class="file-sortable-handle" v-if="sortable === 'handle'">
-          <slot name="sortable-handle">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
-          </slot>
-        </span>
-        <slot name="file-preview" :fileData="fileData" :index="index">
-          <VueFilePreview 
-            :value="fileData" :deletable="isDeletable" :editable="editable === true" :linkable="linkable === true" :errorText="errorText" :disabled="disabled"
-            :thumbnailSize="thumbnailSize" @remove="removeFileData($event)"
-             @rename="filenameChanged($event)" class=""></VueFilePreview>
-        </slot>
-      </component>
-    <!-- </template> -->
-    <template v-if="canAddMore">
-      <slot name="file-preview-new">
-        <div key="new" class="file-preview-wrapper grid-box-item grid-block file-preview-new">
-          <span class="file-preview">
-            <span style="position: absolute; top: 0; right: 0; bottom: 0; left: 0;">
-              <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="100px" y="0px" viewBox="0 0 1000 1000" enable-background="new 0 0 1000 1000" xml:space="preserve">
-                <path d="M745,353c-5.6,0-11.3,0.2-17.2,0.7C687.4,237.3,577.8,157,451,157c-162.1,0-294,131.9-294,294c0,2.1,0,4.1,0,6.2C72.6,479,10,555.8,10,647c0,108.1,87.9,196,196,196h245V618.3l-63.4,63.4c-9.6,9.6-22.1,14.4-34.6,14.4s-25.1-4.8-34.6-14.4c-19.2-19.2-19.2-50.1,0-69.3l147-147c4.6-4.6,9.9-8.1,16-10.6c12-4.9,25.5-4.9,37.4,0c6,2.5,11.4,6.1,16,10.6l147,147c19.2,19.2,19.2,50.1,0,69.3c-9.6,9.6-22.1,14.4-34.6,14.4s-25.1-4.8-34.6-14.4L549,618.3V843h196c135.1,0,245-109.9,245-245S880.1,353,745,353z"/>
-              </svg>
-              <span class="help-text">{{ helpTextComputed }}</span>
+      <component
+        :is="isSortable ? 'vfa-sortable-list' : 'VueFileList'"
+        v-model="filesData"
+        :axis="theme == 'list' ? 'y' : 'xy'"
+        :appendTo="'#vfa-' + uniqueId + ' .vue-file-agent'"
+        :transitionDuration="transitionDuration"
+        :pressDelay="sortable === 'hold' ? 200 : 0"
+        :useDragHandle="sortable === 'handle'"
+        @sort-start="sortStart()"
+        @sort-end="sortEnd($event)"
+        :helperClass="'active-sorting-item'"
+      >
+        <transition-group name="grid-box" tag="div" class="">
+          <!-- <template v-for="(fileData, index) in filesData"> -->
+          <component
+            :is="isSortable ? 'vfa-sortable-item' : 'VueFileListItem'"
+            v-for="(fileData, index) in filesData"
+            class="file-preview-wrapper grid-box-item grid-block"
+            :index="index"
+            :key="fileData.id"
+          >
+            <span v-vfa-sortable-handle class="file-sortable-handle" v-if="sortable === 'handle'">
+              <slot name="sortable-handle">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                  <path d="M0 0h24v24H0z" fill="none" />
+                  <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+                </svg>
+              </slot>
             </span>
-          </span>
-        </div>
-      </slot>
-    </template>
-  </transition-group>
-  </component>
-    <input title="" :disabled="disabled === true || (hasMultiple && !canAddMore)" ref="fileInput" type="file" :multiple="hasMultiple" class="file-input" @change="filesChanged" :accept="accept || '*'">
+            <slot name="file-preview" :fileData="fileData" :index="index">
+              <VueFilePreview
+                :value="fileData"
+                :deletable="isDeletable"
+                :editable="editable === true"
+                :linkable="linkable === true"
+                :errorText="errorText"
+                :disabled="disabled"
+                :thumbnailSize="thumbnailSize"
+                @remove="removeFileData($event)"
+                @rename="filenameChanged($event)"
+                class=""
+              ></VueFilePreview>
+            </slot>
+          </component>
+          <!-- </template> -->
+          <template v-if="canAddMore">
+            <slot name="file-preview-new">
+              <div key="new" class="file-preview-wrapper grid-box-item grid-block file-preview-new">
+                <span class="file-preview">
+                  <span style="position: absolute; top: 0; right: 0; bottom: 0; left: 0;">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      xmlns:xlink="http://www.w3.org/1999/xlink"
+                      version="1.1"
+                      x="100px"
+                      y="0px"
+                      viewBox="0 0 1000 1000"
+                      enable-background="new 0 0 1000 1000"
+                      xml:space="preserve"
+                    >
+                      <path
+                        d="M745,353c-5.6,0-11.3,0.2-17.2,0.7C687.4,237.3,577.8,157,451,157c-162.1,0-294,131.9-294,294c0,2.1,0,4.1,0,6.2C72.6,479,10,555.8,10,647c0,108.1,87.9,196,196,196h245V618.3l-63.4,63.4c-9.6,9.6-22.1,14.4-34.6,14.4s-25.1-4.8-34.6-14.4c-19.2-19.2-19.2-50.1,0-69.3l147-147c4.6-4.6,9.9-8.1,16-10.6c12-4.9,25.5-4.9,37.4,0c6,2.5,11.4,6.1,16,10.6l147,147c19.2,19.2,19.2,50.1,0,69.3c-9.6,9.6-22.1,14.4-34.6,14.4s-25.1-4.8-34.6-14.4L549,618.3V843h196c135.1,0,245-109.9,245-245S880.1,353,745,353z"
+                      />
+                    </svg>
+                    <span class="help-text">{{ helpTextComputed }}</span>
+                  </span>
+                </span>
+              </div>
+            </slot>
+          </template>
+        </transition-group>
+      </component>
+      <input
+        title=""
+        :disabled="disabled === true || (hasMultiple && !canAddMore)"
+        ref="fileInput"
+        type="file"
+        :multiple="hasMultiple"
+        class="file-input"
+        @change="filesChanged"
+        :accept="accept || '*'"
+      />
 
-    <slot name="after-inner"></slot>
-  </div>
-  <slot name="after-outer"></slot>
+      <slot name="after-inner"></slot>
+    </div>
+    <slot name="after-outer"></slot>
   </div>
 </template>
 <style lang="scss">
-  @import "../scss/vue-file-agent.scss";
+@import '../scss/vue-file-agent.scss';
 </style>
 <script lang="ts">
-  import Vue from 'vue';
-  import mixin from './vue-file-agent-mixin';
+import Vue from 'vue';
+import mixin from './vue-file-agent-mixin';
 
-  export default Vue.extend({
-    mixins: [mixin],
-  });
+export default Vue.extend({
+  mixins: [mixin],
+});
 </script>
--- src/components/vue-file-preview.vue
+++ src/components/vue-file-preview.vue
@@ -1,42 +1,111 @@
 <template>
-  <div :class="['file-preview-wrapper-' + fileData.ext(), fileData.isImage() ? 'file-preview-wrapper-image' : 'file-preview-wrapper-other', 'file-category-' + fileData.icon().category, {'file-is-playing-av': fileData.isPlayingAv}, {'is-deletable': deletable === true}, {'is-editable': editable === true}, {'is-edit-input-focused': isEditInputFocused}, {'has-error': fileData.error}]">
+  <div
+    :class="[
+      'file-preview-wrapper-' + fileData.ext(),
+      fileData.isImage() ? 'file-preview-wrapper-image' : 'file-preview-wrapper-other',
+      'file-category-' + fileData.icon().category,
+      { 'file-is-playing-av': fileData.isPlayingAv },
+      { 'is-deletable': deletable === true },
+      { 'is-editable': editable === true },
+      { 'is-edit-input-focused': isEditInputFocused },
+      { 'has-error': fileData.error },
+    ]"
+  >
     <div class="file-error-wrapper" v-if="fileData.error" @click="dismissError()">
       <div class="file-error-message file-error-message-client">
         {{ fileData.getErrorMessage(errorText) }}
       </div>
     </div>
     <div ref="wrapper" class="file-av-wrapper" v-if="fileData.isPlayableAudio() || fileData.isPlayableVideo()">
-        <div class="file-av-action" @click="playAv(fileData)">
-          <span class="file-av-stop">
-            <svg width="24" height="24" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-          </span>
-          <span class="file-av-play">
-            <svg width="48" height="48" viewBox="0 0 48 48"><path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path></svg>
-          </span>
-        </div>
+      <div class="file-av-action" @click="playAv(fileData)">
+        <span class="file-av-stop">
+          <svg width="24" height="24" viewBox="0 0 24 24">
+            <path
+              d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+            />
+            <path d="M0 0h24v24H0z" fill="none" />
+          </svg>
+        </span>
+        <span class="file-av-play">
+          <svg width="48" height="48" viewBox="0 0 48 48">
+            <path d="M24 4C12.95 4 4 12.95 4 24s8.95 20 20 20 20-8.95 20-20S35.05 4 24 4zm-4 29V15l12 9-12 9z"></path>
+          </svg>
+        </span>
+      </div>
     </div>
-    <span class="file-preview" :class="{'image-preview': fileData.isImage(), 'other-preview': !fileData.isImage(), 'dark-content': fileData.isImage() && fileData.isDarkColor()}" :style="{'background-color': fileData.color(), 'background-imagex': 'url(' + fileData.src() + ')', widthx: fileData.width + 'px', heightx: fileData.height + 'px'}">
+    <span
+      class="file-preview"
+      :class="{
+        'image-preview': fileData.isImage(),
+        'other-preview': !fileData.isImage(),
+        'dark-content': fileData.isImage() && fileData.isDarkColor(),
+      }"
+      :style="{
+        'background-color': fileData.color(),
+        'background-imagex': 'url(' + fileData.src() + ')',
+        widthx: fileData.width + 'px',
+        heightx: fileData.height + 'px',
+      }"
+    >
       <span class="file-preview-overlay"></span>
       <span class="thumbnail" style="position: absolute; top: 0; right: 0; bottom: 0; left: 0; overflow: hidden;">
-        <img v-if="fileData.isImage() || fileData.isPlayableVideo()" class="file-preview-img" :src="fileData.src()">
+        <img v-if="fileData.isImage() || fileData.isPlayableVideo()" class="file-preview-img" :src="fileData.src()" />
       </span>
       <span class="file-ext">{{ fileData.ext() }}</span>
       <span class="file-size">{{ fileData.size() }}</span>
-      <span v-if="deletable" class="file-delete" @click="removeFileData(fileData)" @touchstart="filenameClearPressed()" @mousedown="filenameClearPressed()" >
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+      <span
+        v-if="deletable"
+        class="file-delete"
+        @click="removeFileData(fileData)"
+        @touchstart="filenameClearPressed()"
+        @mousedown="filenameClearPressed()"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+          <path
+            d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"
+          />
+          <path d="M0 0h24v24H0z" fill="none" />
+        </svg>
       </span>
       <span class="file-name" @click="editFileName()">
-        <input class="file-name-input" ref="input" v-if="editable === true" :disabled="disabled === true" type="text" :value="fileData.name(true)" @focus="editInputFocused()" @blur="editInputBlured()" @change="filenameChanged()" @input="filenameChanged()" @keyup.enter="filenameChanged(true)" @keyup.esc="filenameChanged(false)">
+        <input
+          class="file-name-input"
+          ref="input"
+          v-if="editable === true"
+          :disabled="disabled === true"
+          type="text"
+          :value="fileData.name(true)"
+          @focus="editInputFocused()"
+          @blur="editInputBlured()"
+          @change="filenameChanged()"
+          @input="filenameChanged()"
+          @keyup.enter="filenameChanged(true)"
+          @keyup.esc="filenameChanged(false)"
+        />
         <span class="file-name-edit-icon" v-if="editable === true">
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path
+              d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"
+            />
+            <path d="M0 0h24v24H0z" fill="none" />
+          </svg>
         </span>
         <span class="file-name-text">{{ fileData.name(true) }}</span>
       </span>
       <span v-if="fileData.dimensions.width && fileData.dimensions.height" class="image-dimension">
-        <span class="image-dimension-width">{{ fileData.dimensions.width }}</span><span class="image-dimension-height">{{ fileData.dimensions.height }}</span>
+        <span class="image-dimension-width">{{ fileData.dimensions.width }}</span
+        ><span class="image-dimension-height">{{ fileData.dimensions.height }}</span>
       </span>
-      <span class="file-progress" v-if="fileData.hasProgress()" :class="{'file-progress-full': fileData.progress() >= 99.9999, 'file-progress-done': fileData.progress() >= 100, 'has-file-progress': fileData.progress() > 0}">
-        <span class="file-progress-bar" :style="{width: fileData.progress() + '%'}"></span>
+      <span
+        class="file-progress"
+        v-if="fileData.hasProgress()"
+        :class="{
+          'file-progress-full': fileData.progress() >= 99.9999,
+          'file-progress-done': fileData.progress() >= 100,
+          'has-file-progress': fileData.progress() > 0,
+        }"
+      >
+        <span class="file-progress-bar" :style="{ width: fileData.progress() + '%' }"></span>
       </span>
       <span class="file-icon">
         <a v-if="hasLinkableUrl" :href="fileData.url" target="_blank" :title="fileData.name()">
@@ -48,10 +117,10 @@
   </div>
 </template>
 <script lang="ts">
-  import Vue from 'vue';
-  import mixin from './vue-file-preview-mixin';
+import Vue from 'vue';
+import mixin from './vue-file-preview-mixin';
 
-  export default Vue.extend({
-    mixins: [mixin],
-  });
+export default Vue.extend({
+  mixins: [mixin],
+});
 </script>
